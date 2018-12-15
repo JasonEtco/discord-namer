2e5d77e64ddd3bc0bc87e1d2bc606423a1351c37
Jason Etcovitch <jasonetco@github.com>
2018-12-15 14:32:27 -0500

    Comments and cleanup

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const nemojis = require('node-emoji')
 const client = new Discord.Client()
 
 client.on('message', msg => {
+  // Do nothing if the user wasn't mentioned
   if (!msg.mentions.users.has(client.user)) return
-
-  const jason = msg.guild.me
+  // Get a random emoji
   const random = nemojis.random()
+  // Change their nickname
   return msg.guild.me.setNickname(`${random.emoji}man`)
 })
 
