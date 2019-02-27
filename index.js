cab9ba06021de294df5e387b3e54b429fb3ae2ee
Jason Etcovitch <jasonetco@github.com>
2019-02-26 20:21:27 -0500

    Manually set via "I am :emoji:man"

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const Discord = require('discord.js')
 const nemojis = require('node-emoji')
 const client = new Discord.Client()
 
+const CUSTOM_NAME_REGEX = /^I\sam\s(?<emoji>.*)man$/
+
 client.on('message', msg => {
-  // Do nothing if the user wasn't mentioned
-  if (!msg.mentions.users.has(client.user.id)) return
-  // Get a random emoji
-  const random = nemojis.random()
-  // Change their nickname
-  return msg.guild.me.setNickname(`${random.emoji}man`)
+  // I did it!
+  if (msg.author.id === client.user.id) {
+    // Will match `I am :emoji:man`
+    const match = msg.cleanContent.match(CUSTOM_NAME_REGEX)
+    // No matches found
+    if (!match || !match.groups.emoji) return
+    // Change their nickname
+    return msg.guild.me.setNickname(`${match.groups.emoji}man`)
+  }
+
+  // The user wasn't mentioned
+  if (msg.mentions.users.has(client.user.id)) {
+    // Get a random emoji
+    const random = nemojis.random()
+    // Change their nickname
+    return msg.guild.me.setNickname(`${random.emoji}man`)
+  }
 })
 
 client.login(process.env.DISCORD_TOKEN)
